5acdeae46c2b8c7773099f5ef5072e9ab2231519
TheBaytur <bayturcodding@gmail.com>
2024-09-22 01:33:35 +0800

    #3

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -1,16 +1,31 @@
 package com.example.testapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var minimumInput: EditText
+
+    private lateinit var maximumInput: EditText
+
+    private lateinit var resultText: TextView
+
+    private lateinit var generateButton: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+
 
     }
 }
